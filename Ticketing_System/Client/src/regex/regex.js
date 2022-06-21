70ff8e86ff8f9e47a6cffe8928f84b28db06899b
Shivam Shukla <100199959+ShivamShukla20013@users.noreply.github.com>
2022-06-21 12:46:19 +0530

    Delete Ticketing_System directory

--- Ticketing_System/Client/src/regex/regex.js
+++
@@ -1,3 +0,0 @@
-export const validName = new RegExp('^[a-zA-Z ]+$');
-export const validUsername = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{5,15}$');
-export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,16}$');
